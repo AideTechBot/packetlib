430c7bc2d9bb12d6cbcfb6ca565331040c0d3f43
AideTechBot <manimo41@hotmai.com>
2014-04-28 17:19:40 -0300

    Added encode_data function
    It's arguments are:
    data = the data to encode in an array
    sepchar = character that seperates the data

--- packetlib.py
+++ packetlib.py
@@ -5,3 +5,16 @@ a dynamic packet library for encoding and parsing data
 
 written by Aidetechbot
 """
+
+#this returns a string of encoded data
+#data is the data to encode(in an array)
+#sepchar is the character that will seperate the data. 
+#It's useful when you can only use certain chars (ex: baudot)
+def encode_data(data,sepchar):
+	if(sepchar == ""):
+		raise Exception("Invalid seperation char.")
+	encdata = str(sepchar).join(data)
+	message = "START" + str(sepchar) + encdata + str(sepchar) + "END"
+	return message
+
+print encode_data(["abba"],"")
