fe516fa74611fac989855a3a7ab6ca64f0df807b
AideTechBot <mandio0302@gmail.com>
2014-05-01 20:30:25 -0300

    Added two functions.
    error_protect() encodes the data for data protection
    error_correct() decodes and corrects errors

--- packetlib.py
+++ packetlib.py
@@ -5,6 +5,7 @@ a dynamic packet library for encoding and parsing data
 
 written by Aidetechbot
 """
+import collections
 
 #this returns a string of encoded data
 #data is the data to encode(in an array)
@@ -52,3 +53,35 @@ def parse_data(data, sepchar):
 				y = x + 1
 	return parsed
 
+
+#returns a string with the character all multiplied by num
+#ex: "START|HELLO|END" becomes "SSTTAARRTT||HHEELLLLOO||EENNDD"
+def error_protect(data, num):
+	finalstr = ""
+	for x in range(0,len(data)):
+		for y in range(0,num):
+			finalstr = finalstr + data[x]
+	return finalstr
+
+
+#takes the output of the above and does the opposite 
+#also if the string is corrupt ex: "AA.A"
+#it will be returned as "A"
+#if it's "...A" it will become ".", so the more characters you multiply by, the less errors there will be
+#but it will also take more time/bandwidth
+def error_correct(data, num):
+	temp = ""
+	returnstr = ""
+	times = 1
+	for x in range(0,len(data)):
+		temp = temp + data[x]
+		#print "temp", temp
+		#print "num ", num * times
+		#print "x ", x
+		if(not x == 0 and x == (num * times)- 1):
+			char = collections.Counter(temp).most_common(1)[0][0]
+			returnstr = returnstr + char
+			temp = ""
+			times = times + 1
+			#print "-------------------------------", times
+	return returnstr
